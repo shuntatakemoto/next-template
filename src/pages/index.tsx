e92c0c9be9e1519cb770a2141897ec880540834c
haruta <58549977+shuntatakemoto@users.noreply.github.com>
2022-07-17 17:01:30 +0900

    Import by absolute path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from 'next'
+import { Button } from '@/components/Button'
 
 const Home: NextPage = () => {
   return (
     <>
       <div>
         <h1>This is Next.js Template!</h1>
+        <Button />
       </div>
     </>
   )
